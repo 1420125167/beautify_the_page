c9e4c502de5233473f7b5140cb9c5444ed1c9f1a
sur <sur@sur.net.cn>
2022-05-07 22:39:53 +0800

    separate carousel into autoplay.js

--- src/page/home/component/autoplay.js
+++ src/page/home/component/autoplay.js
@@ -1,24 +1,45 @@
 import React, {PureComponent} from 'react';
-import {List, Avatar, Icon} from 'antd';
+import {Carousel, Popover, Card} from 'antd';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {Banner} from "../style";
+import img1 from '../../../statics/home/bg1.jpg';
+import {actionCreators} from "../store";
 
 class Autoplay extends PureComponent {
 	render() {
-		return null
+		const {classData}=this.props;
+		// return null
+		return (
+			<Carousel autoplay>
+				{
+					classData.map((item)=>(
+					<Popover
+						placement="right"
+						content={
+							<Card title={item.get('lesson_name')} style={{width: 300}}>
+								<div dangerouslySetInnerHTML={{__html: item.get('lesson_intro')}}/>
+							</Card>
+						}
+						key={item.get('id')}>
+						<Link to={"/classdetail/"+item.get('id')+"/"+item.get('lesson_chapter')}>
+							<Banner imgSrc={img1}>
+								<h3>{item.get('lesson_name')}</h3>
+							</Banner>
+						</Link>
+					</Popover>
+					))
+				}
+			</Carousel>
+		)
 	}
 }
-
-export default Autoplay
-import React, {PureComponent} from 'react';
-import {List, Avatar, Icon} from 'antd';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-
-class Autoplay extends PureComponent {
-	render() {
-		return null
+const mapState = (state) => ({
+	classData: state.getIn(['home', 'classData'])
+});
+const mapDispatch = (dispatch) => ({
+	loadHomeData() {
+		dispatch(actionCreators.loadHomeData())
 	}
-}
-
-export default Autoplay
+});
+export default connect(mapState, mapDispatch)(Autoplay);
--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,9 +1,7 @@
 import React, {PureComponent} from 'react';
-import {Carousel, Row, Col, BackTop, Popover, Card} from 'antd';
-import {Link} from 'react-router-dom';
+import {Row, Col, BackTop} from 'antd';
 import {connect} from 'react-redux';
-import img1 from '../../statics/home/bg1.jpg';
-import {Content, RightContent, CenterContent, Banner} from './style'
+import {Content, RightContent, CenterContent} from './style'
 import {actionCreators} from './store';
 import Bottom from '../../common/bottom';
 import New from './component/new';
@@ -11,10 +9,10 @@ import Problem from './component/problem';
 import History from './component/rightnav';
 import LeftMenu from '../../common/leftMenu';
 import Header from '../../common/header';
+import Autoplay from "./component/autoplay";
 
 class Home extends PureComponent {
 	render() {
-		const {classData} = this.props;
 		return (
 			<div>
 				<Header/>
@@ -25,27 +23,8 @@ class Home extends PureComponent {
 						</Col>
 						<Col span={12}>
 							<CenterContent>
-								<Carousel autoplay>
-									{
-										classData.map((item) => (
-											<Popover
-												placement="right"
-												content={
-													<Card title={item.get('lesson_name')} style={{width: 300}}>
-														<div dangerouslySetInnerHTML={{__html: item.get('lesson_intro')}}/>
-													</Card>
-												}
-												key={item.get('id')}>
-												<Link to={"/classdetail/" + item.get('id') + "/" + item.get('lesson_chapter')}>
-													<Banner imgSrc={img1}>
-														<h3>{item.get('lesson_name')}</h3>
-													</Banner>
-												</Link>
-											</Popover>
-										))
-									}
-								</Carousel>
-								{/*<Problem/>*/}
+								<Autoplay/>
+								<Problem/>
 							</CenterContent>
 						</Col>
 						<Col span={6}>
