ec755fd8ea1a43b86b4158ae88be7221ed414f77
sur <sur@sur.net.cn>
2022-05-11 18:57:07 +0800

    xadmin functional, panel migration depends on time

--- public/server/AIWebAPI/settings.py
+++ public/server/AIWebAPI/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 # 跨域增加忽略
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ['hhttp://localhost']
+CORS_ORIGIN_WHITELIST = ['http://localhost']
 # CORS_ORIGIN_WHITELIST = ( '*' )
 CORS_ALLOW_METHODS = ( 'DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT', 'VIEW', )
 CORS_ALLOW_HEADERS = ( 'XMLHttpRequest', 'X_FILENAME', 'accept-encoding', 'authorization', 'content-type', 'dnt', 'origin', 'user-agent', 'x-csrftoken', 'x-requested-with', )
